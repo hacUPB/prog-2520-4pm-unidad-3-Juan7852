d7389b8f3726743f067c87b108be67882cefc621
juan7852 <juan.lassoo@upb.edu.co>
2025-10-09 17:27:41 -0500

    reto09

--- ejercicios3.py
+++ ejercicios3.py
@@ -1,5 +1,24 @@
-print(10 > 5)
-print("Hola" != "Mundo")
-print(3.14 <= 4.5)
-nombre = "Juan"
-print(nombre == "Juan")
+edad = int(input("Por favor, ingresa tu edad: "))
+
+if edad >= 0:
+    if edad <= 5:
+        etapa = "Primera Infancia"
+    elif edad <= 11:
+        etapa = "Infancia"
+    elif edad <= 26:
+        if edad <= 18:
+            etapa = "adolecencia"
+        if edad >= 14: 
+            etapa = "juventud"
+    elif edad <= 18:
+        etapa = "Adolescencia"
+    elif edad <= 26:
+        etapa = "Juventud"
+    elif edad <= 59:
+        etapa = "Adultez"
+    else:
+        etapa = "Persona Mayor (Envejecimiento y Vejez)"
+    
+    print(f"Tu etapa del ciclo de vida es: {etapa}")
+else:
+    print("La edad ingresada no es válida.")
